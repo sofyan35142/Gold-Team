ec791c2e22b37b31e733ea0dfc5d7a309ee533a1
Keydrf <112835021+Keydrf@users.noreply.github.com>
2022-11-28 08:37:52 +0700

    up

--- database/seeders/kakomli.php
+++ database/seeders/kakomli.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-class jurusan extends Seeder
+class kakomli extends Seeder
 {
     /**
      * Run the database seeds.
